1bf1ca3bcc7c7cd44ad0311773d93c8a4d02c624
apm <amoosavi@kth.se>
2016-10-31 22:16:43 +0100

    testing recursion

---
+++ src/main/java/Main.java
@@ -0,0 +1,55 @@
+/**
+ * Created by arvid on 2016-10-31.
+ */
+import
+
+public class Main {
+
+    public static void main(String args[]){
+
+        int fWithLoop = computeFactorialWithLoop(3);
+        int fWithRecursion = findFactorialRecursion(3);
+        int fibonacciCalculated = computeFibonacci(6);
+
+        System.out.println(fWithLoop);
+        System.out.println(fWithRecursion);
+        System.out.println(fibonacciCalculated);
+
+
+    }
+
+
+    public static int computeFactorialWithLoop(int n)
+    {
+        int factorial = n;
+        for (int i = n - 1; i >= 1; i--) {
+            factorial = factorial * i;
+        }
+        return factorial;
+    }
+
+
+    public static int findFactorialRecursion(int n)
+    {
+        if ( n == 1 || n == 0) {
+            return 1;
+        } else {
+            return (n * findFactorialRecursion(n-1));
+        }
+    }
+
+
+    public static int computeFibonacci(int n){
+
+        int fibonacci = n;
+
+        if (n == 0 || n == 1) {
+            return n;
+        } else {
+
+            return (fibonacci*(n - 1) + fibonacci*(n - 2));
+        }
+
+    }
+
+}
