f501e91f0301bde8c334480c56306fd7e96eb4f5
apm <amoosavi@kth.se>
2016-11-07 14:26:04 +0100

    designing APIs

---
+++ src/main/java/id1020/laboration1/IterativePascal.java
@@ -0,0 +1,19 @@
+package id1020.laboration1;
+
+/**
+ * Created by arvid on 2016-11-07.
+ */
+public class IterativePascal implements Pascal {
+
+    // Iterative methods goes here
+
+    public void printPascal(int n, boolean reverse) {
+
+    }
+
+    public int binom(int n, int k) {
+
+        return n;
+    }
+
+}
---
+++ src/main/java/id1020/laboration1/Pascal.java
@@ -0,0 +1,11 @@
+package id1020.laboration1;
+
+/**
+ * Created by arvid on 2016-11-07.
+ */
+public interface Pascal {
+
+    public void printPascal (int n, boolean reverse);
+    public int binom (int n, int k);
+
+}
