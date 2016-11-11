6aa69822e4a3e1f71a415769b1fe065a75d48435
apm <amoosavi@kth.se>
2016-11-11 23:18:25 +0100

    final tests

--- src/main/java/id1020/homework/Homework2.java
+++ src/main/java/id1020/homework/Homework2.java
@@ -1,5 +1,7 @@
 package id1020.homework;
 
+import edu.princeton.cs.algs4.Stack;
+
 /**
  * Created by arvid on 2016-11-10.
  */
@@ -23,8 +25,37 @@ public class Homework2 {
         System.out.println();
     }
 
+    public static int test ( int n)
+    {
+        if (n <= 2) return 1;
+        else {
+            System.out.println(n);
+            return test(n-2) * test(n-2);
+        }
+
+    }
+
+    public static int f(int n) {
+        int s = 0;
+        while(n > 1) {
+            n = n/2;
+            s++;
+            System.out.println(n);
+            System.out.println(s);
+        }
+        return s;
+    }
+
     public static void main(String args[]){
 
+        // converts: (2+((3+4)*(5*6))) into: 2 3 4 + 5 6 * * +
+        char[] chars = new char[]{'(','2','+','(','(','3','+','4',')','*','(','5','*','6',')',')',')'};
+        fun(chars);
+        System.out.println();
+
+        //test(60);
+
+        f(8);
         
     }
 
