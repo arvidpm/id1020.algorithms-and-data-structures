aafe9f6c63bf8d16c7e415c510ab506428c3dff2
apm <amoosavi@kth.se>
2016-11-23 12:24:40 +0100

    initial lab 4 files

---
+++ lab-4/src/main/java/Driver.java
@@ -0,0 +1,42 @@
+/**
+ * Created by arvid on 2016-11-23.
+ */
+import edu.princeton.cs.introcs.In;
+import java.net.URL;
+
+public class Driver {
+
+    public static void main(String[] args) {
+        URL url = ClassLoader.getSystemResource("kap1.txt");
+
+        if (url != null) {
+            System.out.println("Reading from: " + url);
+        } else {
+            System.out.println("Couldn't find file: kap1.txt");
+        }
+
+        In input = new In(url);
+
+        while (!input.isEmpty()) {
+            String line = input.readLine().trim();
+            String[] words = line.split("(\\. )|:|,|;|!|\\?|( - )|--|(\' )| ");
+            String lastOfLine = words[words.length - 1];
+
+            if (lastOfLine.endsWith(".")) {
+                words[words.length - 1] = lastOfLine.substring(0, lastOfLine.length() - 1);
+            }
+
+            for (String word : words) {
+                String word2 = word.replaceAll("\"|\\(|\\)", "");
+
+                if (word2.isEmpty()) {
+                    continue;
+                }
+
+                System.out.println(word2);
+                // Add the word to the trie
+            }
+        }
+        //Perform analysis
+    }
+}
