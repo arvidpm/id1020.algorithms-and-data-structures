c76837448396b4278a1f30687c40a7e56a617e2a
apm <amoosavi@kth.se>
2016-12-05 22:47:44 +0100

    formatting, some comments

--- project-1/src/main/java/Engine/TinySearchEngine.java
+++ project-1/src/main/java/Engine/TinySearchEngine.java
@@ -12,48 +12,41 @@ import java.util.List;
 /**
  * Created by arvid on 2016-12-01.
  */
+
 class TinySearchEngine implements TinySearchEngineBase {
 
-    private ArrayList<ArrayList<Words>> words = new ArrayList<ArrayList<Words>>();
+    private ArrayList<ArrayList<Words>> documentWords = new ArrayList<ArrayList<Words>>();
     private ArrayList<Document> docs = new ArrayList<Document>();
 
-    private class Words {
-        private String word;
-        private ArrayList<Attributes> attr = new ArrayList<Attributes>();
+    public static void main(String[] args) throws Exception {
 
-        private Words(Word word, Attributes attr) {
-            this.word = word.word;
-            this.attr.add(attr);
-        }
-    }
+        TinySearchEngineBase searchEngine = new TinySearchEngine();
+        Driver.run(searchEngine);
 
-    // Used to hold search results while processing
-    private class Docs {
-        private Document doc;
-        private ArrayList<Words> word = new ArrayList<Words>();
-
-        public Docs(Document doc, Words word) {
-            this.doc = doc;
-            this.word.add(word);
-        }
     }
 
     public void insert(Word word, Attributes attr) {
 
         ArrayList<Words> docWords;
 
+        /* Existing document */
         if (docs.contains(attr.document)) {
 
             int index = docs.indexOf(attr.document);
-            docWords = words.get(index);
+            docWords = documentWords.get(index);
 
         } else {
-
+            /* New document */
             docs.add(attr.document);
             docWords = new ArrayList<Words>();
-            words.add(docWords);
+            documentWords.add(docWords);
         }
 
+        /*
+        *  Add the word to the collection by using Java compareTo method
+        *  Checks for duplicates and if so adds attributes to the index.
+        *  If the word is bigger we insert at the given index, thus sorting.
+        * */
         for (int index = 0; index <= docWords.size(); index++) {
 
             if (docWords.size() == index) {
@@ -75,9 +68,9 @@ class TinySearchEngine implements TinySearchEngineBase {
         }
     }
 
-
     public List<Document> search(String query) {
 
+        /* For multiple query words */
         String[] queries = query.split(" ");
 
         boolean sort = false;
@@ -86,6 +79,10 @@ class TinySearchEngine implements TinySearchEngineBase {
 
         List<Docs> res = new ArrayList<Docs>();
 
+        /*
+        * If array > 3, and the first word after our query words is 'orderby', we run the body.
+        * By setting 'orderby' element to null we break the for-loop when all words
+        * */
         if (queries.length > 3 && queries[queries.length - 3].equalsIgnoreCase("orderby")) {
 
             sort = true;
@@ -102,37 +99,39 @@ class TinySearchEngine implements TinySearchEngineBase {
             queries[queries.length - 3] = null;
         }
 
-
         for (String q : queries) {
 
             if (q == null) break;
 
-            for (int i = 0; i < words.size(); i++) {
+            /* Searching all documents */
+            for (int i = 0; i < documentWords.size(); i++) {
 
-                int index = binarySearch(q, words.get(i));
+                /* Searching for word q in document i */
+                int index = binarySearch(q, documentWords.get(i));
+
+                /* If we got a hit */
                 if (index != -1) {
 
-                    Docs documentContainer = null;
+                    /* If the current document already is in the resultset, we find the reference */
+                    Docs docContainer = null;
+                    for (Docs resItem : res) {
 
-                    for (Docs tmpDocumentContainer : res) {
-
-                        if (tmpDocumentContainer.doc.equals(docs.get(i))) {
-                            documentContainer = tmpDocumentContainer;
+                        if (resItem.doc.equals(docs.get(i))) {
+                            docContainer = resItem;
                             break;
                         }
                     }
 
-                    if (documentContainer == null) {
-                        res.add(new Docs(docs.get(i), words.get(i).get(index)));
-                    } else {
-                        documentContainer.word.add(words.get(i).get(index));
-                    }
+                    /* if: we find a result in a document (first time)
+                     * else: document already part of results
+                     * */
+                    if (docContainer == null) res.add(new Docs(docs.get(i), documentWords.get(i).get(index)));
+                    else docContainer.word.add(documentWords.get(i).get(index));
                 }
             }
         }
 
-        List<Document> results = new ArrayList<Document>();
-
+        /* Unimplemented sorting of results by properties in selected order */
         if (sort) {
             if (prop == 1) System.out.println("Should be sorted by count.");
             if (prop == 2) System.out.println("Should be sorted by popularity.");
@@ -141,40 +140,58 @@ class TinySearchEngine implements TinySearchEngineBase {
             if (order == 2) System.out.println("Should be ordered descending.");
         }
 
+        /* Results to be returned */
+        List<Document> results = new ArrayList<Document>();
+
         for (Docs documentContainer : res) {
-            if (!results.contains(documentContainer.doc)) {
-                results.add(documentContainer.doc);
-            }
+            if (!results.contains(documentContainer.doc)) results.add(documentContainer.doc);
         }
         return results;
     }
 
-
+    /* A modified binarySearch from lab 1.
+    * Comparision is done with compareTo, in the interval 0 - (ArrayList-1)
+    * If (cmp > 0) we set hi to mid-1. If (cmp < 0) we set low to mid+1.
+    * If word is found we return mid (index), else -1.
+    * */
     private int binarySearch(String word, ArrayList<Words> Words) {
-        if (Words.size() == 0) { return -1; }
+
+        if (Words.size() == 0) return -1;
 
         int lo = 0;
         int hi = Words.size() - 1;
 
         while (lo <= hi) {
 
-            int mid = lo+(hi-lo)/2;
+            int mid = lo + (hi - lo) / 2;
             String midWord = Words.get(mid).word;
 
             int cmp = midWord.compareTo(word);
 
-            if (cmp > 0) { hi = mid - 1; }
-            else if (cmp < 0) { lo = mid + 1; }
-            else { return mid; }
+            if (cmp > 0) hi = mid - 1;
+            else if (cmp < 0) lo = mid + 1;
+            else return mid;
         }
         return -1;
     }
 
-    
-    public static void main(String[] args) throws Exception {
+    private class Words {
+        private String word;
+        private ArrayList<Attributes> attr = new ArrayList<Attributes>();
 
-        TinySearchEngineBase searchEngine = new TinySearchEngine();
-        Driver.run(searchEngine);
+        private Words(Word word, Attributes attr) {
+            this.word = word.word;
+            this.attr.add(attr);
+        }
+    }
 
+    private class Docs {
+        private Document doc;
+        private ArrayList<Words> word = new ArrayList<Words>();
+
+        private Docs(Document doc, Words word) {
+            this.doc = doc;
+            this.word.add(word);
+        }
     }
 }
