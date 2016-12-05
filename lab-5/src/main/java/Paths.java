cceb2ecd4649f00c9555a2534d51c291d2e25028
apm <amoosavi@kth.se>
2016-12-05 21:15:10 +0100

    initial commit

---
+++ lab-5/src/main/java/Paths.java
@@ -0,0 +1,12 @@
+import se.kth.id1020.Graph;
+import se.kth.id1020.DataSource;
+/**
+ * Created by arvid on 2016-12-05.
+ */
+
+public class Paths {
+    public static void main(String[] args) {
+        Graph g = DataSource.load();
+        // work on g
+    }
+}
